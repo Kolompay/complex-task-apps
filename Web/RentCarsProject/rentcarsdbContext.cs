ab04df5580c4294d8391a940cef35be1da6d220d
fluffystar43 <77292319+fluffystar43@users.noreply.github.com>
2021-05-30 14:25:19 +0300

    Обновлено

--- Web/RentCarsProject/rentcarsdbContext.cs
+++ Web/RentCarsProject/rentcarsdbContext.cs
@@ -37,7 +37,6 @@ namespace RentCarsProject
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
                 optionsBuilder.UseNpgsql("Server=localhost;Port=5432;Database=rentcarsdb;User Id=postgres;Password=pass;");
             }
         }
